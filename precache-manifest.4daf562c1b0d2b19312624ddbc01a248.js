05a7efff5ac604d6bc6db38a35a1c7aeac8cd918
James DiStefano <jamesdistefano@MacBook-Pro.home>
2020-08-12 14:23:52 -0400

    Updates

---
+++ precache-manifest.4daf562c1b0d2b19312624ddbc01a248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68baeafaafb189fd83e807b707fffe3",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "35d9940dc15ca8297754",
+    "url": "/connect4/static/css/main.be8ef2da.chunk.css"
+  },
+  {
+    "revision": "71aff40ea0170baef5a0",
+    "url": "/connect4/static/js/2.61f33547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/connect4/static/js/2.61f33547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d9940dc15ca8297754",
+    "url": "/connect4/static/js/main.e7ec07c2.chunk.js"
+  },
+  {
+    "revision": "86a35434b74698728077",
+    "url": "/connect4/static/js/runtime-main.69a0ef66.js"
+  }
+]);
